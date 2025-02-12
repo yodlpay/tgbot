336e34fe83f1ff18d57f081af1b497d17ba28278
Andreas Oee <andy.oeee@gmail.com>
2025-02-12 14:56:22 +0700

    fix start command

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,21 +1,18 @@
-import { about } from './about';
 import createDebug from 'debug';
-import { subscribe } from './subscribe/index';
+import { subscribe } from './subscribe';
+import { menu } from './menu';
 
 const debug = createDebug('bot:start_command');
 
 export const start = () => async (ctx: any) => {
   const { payload } = ctx;
-  if (!payload) return;
 
-  if (payload.startsWith('subscribe_')) {
+  if (payload?.startsWith('subscribe_')) {
     const base64Ens = payload.replace('subscribe_', '');
     const ensOrAddress = atob(base64Ens);
     ctx.message.text = `/subscribe ${ensOrAddress}`;
     return subscribe()(ctx);
-  } else if (payload.startsWith('about')) {
-    return about()(ctx);
   } else {
-    ctx.reply('Unknown command');
+    return menu()(ctx);
   }
 };
